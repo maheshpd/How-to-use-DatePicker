a8159a1eb7083e47157c0087d1f894933976e2bd
Mahesh Prasad <mahesh223prasad@gmail.com>
2020-05-26 14:55:50 +0530

    done

--- How to use DatePicker/ViewController.swift
+++ How to use DatePicker/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func getDate(_ sender: UIButton) {
+        label.text = "\(datePicker.date)"
+    }
+    
 
 }
 
